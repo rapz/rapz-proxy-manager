d5299ba7a9cb859d7f0dba8fe61c51035ddd1839
rapz <cesar.a.cavalcanti@gmail.com>
2023-12-14 12:07:51 -0300

    asd

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (_, res) => {
-    res.send("HEALTHCHECK OK! v2")
+    res.send("HEALTHCHECK OK! v3")
 })
 
 app.listen(5100)
